16ce81ce6b0f3e524c7ba5e47a35be9e81d38f51
tg0xff <tg0xff@proton.me>
2024-08-02 17:56:56 -0600

    Define buildTree() method

--- classes.js
+++ classes.js
@@ -7,7 +7,15 @@ class Node {
 }
 
 class Tree {
-  constructor() {
-    this.root = this.#buildTree();
+  constructor(array) {
+    this.root = this.buildTree(array, 0, array.length - 1);
+  }
+  buildTree(array, start, end) {
+    if (start > end) return null;
+    const mid = Math.floor((start + end) / 2);
+    const node = new Node(arr[mid]);
+    node.left = this.buildTree(array, start, mid - 1);
+    node.right = this.buildTree(array, mid + 1, end);
+    return node;
   }
 }
