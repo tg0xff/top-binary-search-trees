8226890bf9a85fa02e4bf6ed3d4cb0e6eaa6a102
tg0xff <tg0xff@proton.me>
2024-08-03 16:11:46 -0600

    Fix reference to array in buildTree

--- classes.js
+++ classes.js
@@ -14,7 +14,7 @@ class Tree {
   buildTree(array, start, end) {
     if (start > end) return null;
     const mid = Math.floor((start + end) / 2);
-    const node = new Node(arr[mid]);
+    const node = new Node(array[mid]);
     node.left = this.buildTree(array, start, mid - 1);
     node.right = this.buildTree(array, mid + 1, end);
     return node;
